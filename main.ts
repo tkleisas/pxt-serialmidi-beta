0e3dcaaa8d4c55a0e2d78a2a8f314dc7bba22ee1
Tasos Kleisas <tasos.kleisas@gmail.com>
2018-07-04 01:06:49 +0300

    testing 123

--- main.ts
+++ main.ts
@@ -3,8 +3,8 @@ namespace serialmidi
 {
     let bufr = pins.createBuffer(3);
     //% blockId=send_note_on 
-    //% block="sendNoteOn Ch%channel|Note %note|Velocity %velocity"
-    export function sendNoteOn(channel:number, note:number, velocity:number): void
+    //% block="sendNoteOn Ch $channel|Note $note|Velocity $velocity"
+    export function sendNoteOn(channel:number, note:number, velocity:number)
     {
         bufr.setNumber(NumberFormat.Int8LE, 0, 144 + channel);
         bufr.setNumber(NumberFormat.Int8LE, 1, note);
@@ -13,8 +13,8 @@ namespace serialmidi
     }
     
     //% blockId=send_midi_cc
-    //% block="sendCC Ch%channel|controller %controller|value %value"
-    export function sendCC(channel: number, controller: number, value: number): void
+    //% block="sendCC Ch $channel|controller $controller|value $value"
+    export function sendCC(channel: number, controller: number, value: number)
     {
         bufr.setNumber(NumberFormat.Int8LE, 0, 176 + channel);
         bufr.setNumber(NumberFormat.Int8LE, 1, controller);
